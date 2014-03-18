19a9de7dd8d89ff8737d2eebb4e11240ff1fb294
yllcompany <yll.company@gmail.com>
2014-03-17 17:51:13 -0700

    fix bug with recaptcha

--- public/js/controllers/users.js
+++ public/js/controllers/users.js
@@ -2,6 +2,7 @@ angular.module('mean.users').controller('UsersController', function($scope, $rou
   $scope.global = Global;
 	$scope.zombie = Page.isZombie();
 	$scope.user = {};
+	$scope.vcRecaptcha = {};
 
 	$scope.signIn = function() {
 		Auth.login($scope.user, function() {
@@ -17,23 +18,22 @@ angular.module('mean.users').controller('UsersController', function($scope, $rou
 	};
 
 	$scope.signUp = function() {
-		//console.log($scope.user);
-		$scope.user.recaptcha = $scope.vcRecaptchaService.data();
+		$scope.user.recaptcha = $scope.vcRecaptcha.service.data();
 		Auth.create($scope.user, function() {
 			$location.path('/');
 		},
 		function(data,status,headers) {
 			if(status === 400){
-				console.log(data);
 				$scope.errors = data.errors;
 				$scope.showErrors = true;
 				//Reload recaptcha
-				$scope.vcRecaptchaService.reload();
+				$scope.vcRecaptcha.service.reload();
 			}
 		});
 	};
 });
 
 angular.module('mean.users').controller('RecaptchaCtrl', function($scope, vcRecaptchaService) {
-	$scope.$parent.vcRecaptchaService = vcRecaptchaService;
+	//make vcRecaptchaService available for parent scope
+	$scope.$parent.vcRecaptcha.service = vcRecaptchaService;
 });
