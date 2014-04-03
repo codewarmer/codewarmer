0e73af736036825768ac078ddc4579acce0fcd54
yllcompany <yll.company@gmail.com>
2014-04-02 22:37:28 -0700

    add created field

--- public/js/controllers/articles.js
+++ public/js/controllers/articles.js
@@ -2,7 +2,7 @@ angular.module('mean.articles').controller('ArticlesController', function ($scop
   $scope.global = Global;
 	$scope.user = Auth.getCurrentUser();
 	$scope.checkAccess = Auth.checkAccess;
-	$scope.article = {};
+	$scope.article = {created: (new Date).toISOString()};
 	
 	var path = $location.path();
 	if(path === "/" || $routeParams.tags || $routeParams.search)
@@ -52,12 +52,7 @@ angular.module('mean.articles').controller('ArticlesController', function ($scop
 	};
 
 	function createArticle() {
-    var article = new Articles({
-      title: $scope.article.title,
-			lead: $scope.article.lead,
-      content: $scope.article.content,
-			tags: $scope.article.tags
-    });
+    var article = new Articles($scope.article);
     article.$save(function(response) {
       $location.path("posts/" + response.slug);
     });
