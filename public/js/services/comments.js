0e3daee0a7a18f14d16923038504beb33ccb275c
yllcompany <yll.company@gmail.com>
2014-05-20 23:32:26 -0700

    fix bug with comment submit

--- public/js/services/comments.js
+++ public/js/services/comments.js
@@ -1,6 +1,6 @@
 //Comments Service for REST backend
 angular.module('mean.comments').factory('Comments',function($resource) {
-  return $resource('comments/:commentId',{
+  return $resource('/comments/:commentId',{
 		commentId: '@_id'
 	},{
 		update: {method: 'PUT'}
