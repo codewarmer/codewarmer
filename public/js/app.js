2bfded1a49aae77ea07d21cb0ba4d88a8337c62a
yllcompany <yll.company@gmail.com>
2014-05-20 23:29:10 -0700

    cleanup and commenting

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,20 @@
-var dependencies = ['ngCookies', 
-										'ngResource', 
-										'ngRoute', 
-										'ngAnimate', 
-										'ui.bootstrap', 
-										'ui.route', 
-										'angulartics', 
-										'angulartics.google.analytics', 
-										'mean.system', 
+var dependencies = ['ngCookies',
+										'ngResource',
+										'ngRoute',
+										'ngAnimate',
+										'ui.bootstrap',
+										'ui.route',
+										'angulartics',
+										'angulartics.google.analytics',
+										'mean.system',
 										'mean.articles',
-										'mean.users', 
-										'mean.auth', 
-										'mean.comments', 
-										'mean.admin', 
+										'mean.users',
+										'mean.auth',
+										'mean.comments',
+										'mean.admin',
+										'mean.filters',
 										'AutoFillSync', //provides sync for stored password and autofill
-										'Prettify', 
-										'TimeAgo', // Contains timeago filter
+										'Prettify',
 										'passwordGenerator',
 										'ZeroClip',
 										'AddThis',
@@ -25,5 +25,5 @@ angular.module('mean.system', []);
 var ma = angular.module('mean.articles', ['ngSanitize', 'Compile']);
 angular.module('mean.users', []);
 angular.module('mean.auth', []);
-angular.module('mean.comments', ['NewLines', 'ngAnimate']);
+angular.module('mean.comments', ['ngAnimate']);
 angular.module('mean.admin', []);
--- public/js/filters.js
+++ public/js/filters.js
@@ -1,27 +1,27 @@
-angular.module('TimeAgo', []).filter('timeago',function() {
+angular.module('mean.filters', []).filter('timeago',function() {
   return function(input) {
 		var difference = (new Date()).getTime() - (new Date(input)).getTime();
-		//reduce to seconds
+		//reduce to the seconds
 		difference = Math.round(difference/1000);
-		
+
 		var time = {'year': 31536000, 'month': 2592000, 'day': 86400, 'hour': 3600, 'minute': 60, 'second': 1};
 		var out = [];
 
-		//iteration thru time object
+		//iteration thru the time object
 		for(var key in time){
 			if(time.hasOwnProperty(key)){
-				//find if difference contains at least one amount of time element
+				//find if the difference contains at least one amount of time elements
 				var val = Math.floor(difference/time[key]);
 				if(val>0){
-					//add to out array string as "3 years" or "1 minute"
+					//add to the output array a string that looks like "3 years" or "1 minute"
 					out.push(val+' '+key+(val>1 ? 's' : ''));
-					//reduce difference variable on amount that we already add to out
+					//reduce the difference variable on the amount that has been added
 					difference -= val*time[key];
 				}
-				//break from the loop if second iteration after success 
+				// break from the loop if it's a second iteration after success
 				// to prevent output as "1 month 56 seconds"
 				else if(out.length===1) break;
-				//break from the loop with 2 elements in out
+				// break from the loop with 2 elements in output
 				// to prevent "1 month 5 days 3 hours 36 minutes 5 seconds"
 				if(out.length===2) break;
 			}
@@ -31,17 +31,16 @@ angular.module('TimeAgo', []).filter('timeago',function() {
 		else
 			return 'now';
 	};
-});
-
-//Filter replaces "\n" on "<br/>"
-angular.module('NewLines', []).filter('newlines', function() {
+})
+//Filter replaces "\n" by "<br/>"
+.filter('newlines', function() {
   return function(input) {
 		return input.replace(/\n/g, '<br/>');
 	};
 })
-//Filter replaces HTML "<,>,&" on their htmlentities
+//Filter replaces HTML "<,>,&" by htmlentities
 .filter('noHTML',function() {
   return function(input) {
 		return input.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
 	};
-});	
+});
