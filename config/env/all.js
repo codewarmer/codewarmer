c3e2cba196ce2e873a44c2b8e5b4c9fe593e8a69
yllcompany <yll.company@gmail.com>
2014-05-28 17:01:42 -0700

    added env variables for SMTP relay

--- config/env/all.js
+++ config/env/all.js
@@ -3,5 +3,12 @@ rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
 	root: rootPath,
-	port: process.env.PORT || 3000
+	port: process.env.PORT || 3000,
+	smtpRelay: {
+		service: process.env.SMTP_SERVICE,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASS
+		}
+	}
 }
